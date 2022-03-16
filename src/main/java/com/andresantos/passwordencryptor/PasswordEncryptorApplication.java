5908f317da599ae4f7428dc1e1935121e388b7fb
Andre Santos <andremeirati@gmail.com>
2022-03-16 01:23:26 -0300

    Add database configuration

---
+++ src/main/java/com/andresantos/passwordencryptor/PasswordEncryptorApplication.java
@@ -0,0 +1,13 @@
+package com.andresantos.passwordencryptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordEncryptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordEncryptorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andresantos/passwordencryptor/PasswordEncryptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresantos.passwordencryptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordEncryptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
