dec3a44d4e6f8e89355643d097c5d852e718d343
Andre Santos <andremeirati@gmail.com>
2022-03-16 18:22:28 -0300

    Change auto configuration

--- src/main/java/com/andresantos/passwordencryptor/model/UserModel.java
+++ src/main/java/com/andresantos/passwordencryptor/model/UserModel.java
@@ -1,5 +1,6 @@
 package com.andresantos.passwordencryptor.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,5 +18,6 @@ public class UserModel {
     private Integer id;
     @Column(unique = true)
     private String login;
+    @JsonProperty(access =  JsonProperty.Access.WRITE_ONLY)
     private String password;
 }
